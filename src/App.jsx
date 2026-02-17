05500b4e0c2f1d7224e45b70cbd4e630b3292267
pavel.bednyakov <pavel.bednyakov@rt.ru>
2026-02-17 14:41:53 +0300

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,48 @@
 import './App.css';
 
-import JournalItem from './components/Button/JournalItem/JournalItem.jsx';
-import Button from './components/Button/Button.jsx';
-import CardButton from './components/CardButton/CardButton.jsx';
-import LeftPanel from './layouts/LeftPanel/LeftPanel.jsx';
-import JournalList from './components/JournalList/JournalList.jsx';
-import Body from './layouts/Body/Body.jsx';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton.jsx'
+import JournalList from './components/JournalList/JournalList.jsx';
+import JournalForm from './components/JournalForm/JournalForm.jsx';
+import LeftPanel from './layouts/LeftPanel/LeftPanel.jsx';
 import Header from './components/Header/Header.jsx'
+import Body from './layouts/Body/Body.jsx';
 import { useState } from 'react';
 
+const INITIAL_DATA = [
+    // {
+    //     id: 1,
+    //     title: 'Подготовка к обновлению курсов',
+    //     text: 'Горные походы открывают удивительные природные ландшафты, испытывают туристов физически и морально, дают возможность почувствовать себя первопроходцем.',
+    //     date: new Date()
+    // },
+    // {
+    //     id: 2,
+    //     title: 'Различают альпинизм и горный туризм.',
+    //     text: 'Если в альпинизме главная цель – покорение вершины, то горный туризм – это длительное путешествие в горах, связанное с прохождением многочисленных препятствий. В нем огромную роль играет физическая подготовка путешественников, их выносливость, способность переносить большие нагрузки и тяжести в условиях высокогорья. Поход по горам – это следование определенному, заранее продуманному, маршруту через ущелья, перевалы, долины, ледники, озера и водопады. У каждого маршрута своя степень сложности, подразумевающая преодоление разнообразных трудностей рельефа. Основная же цель в горном походе – не покорение вершин, а преодоление перевалов.',
+    //     date: new Date()
+    // }
+]
 function App() {
-	const data = [
-		{
-			title: 'Подготовка к обновлению курсов',
-			text: 'Горные походы открывают удивительные природные ландшафты, испытывают туристов физически и морально, дают возможность почувствовать себя первопроходцем.',
-			date: new Date()
-		},
-		{
-			title: 'Различают альпинизм и горный туризм.',
-			text: 'Если в альпинизме главная цель – покорение вершины, то горный туризм – это длительное путешествие в горах, связанное с прохождением многочисленных препятствий. В нем огромную роль играет физическая подготовка путешественников, их выносливость, способность переносить большие нагрузки и тяжести в условиях высокогорья. Поход по горам – это следование определенному, заранее продуманному, маршруту через ущелья, перевалы, долины, ледники, озера и водопады. У каждого маршрута своя степень сложности, подразумевающая преодоление разнообразных трудностей рельефа. Основная же цель в горном походе – не покорение вершин, а преодоление перевалов.',
-			date: new Date()
-		}
-	];
+    const [items, setItems] = useState(INITIAL_DATA);
 
-    const [inputData, setInputData] = useState('');
-    const inputChange = (event) => {        
-        setInputData(event.target.value)
+    const addItem = (item) => {
+        setItems(oldItems => [...oldItems, {
+            title: item.title,
+            text: item.text,
+            date: new Date(item.date),
+            id: crypto.randomUUID()
+        }])
     }
-
 	return (
 		<div className='app-wrapper'> 
 			<LeftPanel>
 				<Header />
                 <JournalAddButton />
-				<JournalList>
-					<CardButton>
-						<JournalItem
-							title={data[0].title}
-							text={data[0].text}
-							date={data[0].date}
-						/>
-					</CardButton>
-					<CardButton>
-						<JournalItem
-							title={data[1].title}
-							text={data[1].text}
-							date={data[1].date}
-						/>
-					</CardButton>
+				<JournalList items={items}>
 				</JournalList>
 			</LeftPanel>
 			<Body>
-                <input
-                    type='text'
-                    value={inputData}
-                    onChange={inputChange}
-                />
+                <JournalForm onSubmit={addItem}/>
 			</Body>
 		</div>
 	);
--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,11 +1,31 @@
 import './JournalList.css';
+import JournalItem from '../Button/JournalItem/JournalItem.jsx';
+import CardButton from '../CardButton/CardButton.jsx';
 
-function JournalList({children}) {
-	return (
-		<div className='journal-list'>
-            {children}
-		</div>
-	);
+function JournalList({items}) {
+    if (items.length === 0) {
+        return <p>Пусто</p>
+    }
+
+    const sortItems = (a,b) => {
+        if (a.date < b.date) {
+            return 1
+        } else {
+            return -1
+        }
+    }
+
+    return <div className='journal-list'>{
+        items.sort(sortItems).map(el => (
+            <CardButton key={el.id}>
+                <JournalItem
+                    title={el.title}
+                    text={el.text}
+                    date={el.date}
+                />
+            </CardButton>
+        ))
+    }</div>
 }
 
 export default JournalList;
