62a7c9af6b0daf5851ea174b1a998f42168a18ec
pavel.bednyakov <pavel.bednyakov@rt.ru>
2026-03-16 22:28:19 +0300

    style

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,13 +1,45 @@
 import './JournalForm.css';
 
 import Button from '../Button/Button';
+import { useState } from 'react';
 
 function JournalForm({onSubmit}) {
+    const [formValid, setFormValid] = useState({
+        date: true,
+        text: true,
+        title: true
+    })
+
     const addJournalItem = (e) => {
         e.preventDefault();
 
         const formData = new FormData(e.target);
         const formProps = Object.fromEntries(formData);
+
+        let isValidForm = true;
+
+        if (!formProps.title?.trim().length) {
+            setFormValid(state => ({...state, title: false}))
+            isValidForm = false
+        } else {
+            setFormValid(state => ({...state, title: true}))   
+        }
+
+        if (!formProps.text?.trim().length) {
+            setFormValid(state => ({...state, text: false}))
+            isValidForm = false
+        } else {
+            setFormValid(state => ({...state, text: true}))
+        }
+
+        if (!formProps.date) {
+            setFormValid(state => ({...state, date: false}))
+            isValidForm = false
+        } else {
+            setFormValid(state => ({...state, date: true}))
+        }
+
+        if (!isValidForm) return;
 
         onSubmit(formProps)
     }
@@ -19,17 +51,32 @@ function JournalForm({onSubmit}) {
             <input
                 type='title'
                 name='title'
+                style={
+                    {
+                        borderColor: formValid.title ? 'white' : 'red'
+                    }
+                }
             />
             <input
                 type='date'
                 name='date'
+                style={
+                    {
+                        borderColor: formValid.date ? 'white' : 'red'
+                    }
+                }
             />
             <input
                 type='text'
-                name='tag'
+                name='text'
+                style={
+                    {
+                        borderColor: formValid.text ? 'white' : 'red'
+                    }
+                }
             />
             <textarea
-                name='text'
+                name='commit'
             >
             </textarea>
             <Button text="Сохранить"/>
